73ad15a938f655daf6d7bf1aef453c88095e765c
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-09-17 11:38:01 +0300

    init11

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -13,7 +13,20 @@ namespace ContosoUniversity
         {
             var host = CreateHostBuilder(args).Build();
 
-            CreateDbIfNotExists(host);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
 
             host.Run();
         }
